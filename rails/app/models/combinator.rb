6cf48d230f8d696736a002a3d36bc43447bfe308
Aad Versteden <aad@knowified.com>
2014-06-19 17:13:44 +0200

    Base info and class for constructing the combinators.

---
+++ rails/app/models/combinator.rb
@@ -0,0 +1,40 @@
+# = The combinator
+#
+# A combinator combines bits of information extracted by multiple
+# InformationExtractor s into a single result which may be returned to
+# the client.
+#
+# The combinator works much like a reduce operation.  It starts from a
+# base value, and provides an operation to combine it with extra info.
+# The base value is specified in the base_value method, which must be
+# overridden.
+#
+# == Standard implementation
+#
+# We expect the Information Extractors to return JSON by default and
+# combine them by placing them all into a list.
+#
+# The combinator may solve complex problems.  If the combinator works
+# together with the Filter, it may help in the efficient querying of
+# listings.
+#
+# == How to build
+#
+# Add a Combinator to the combinators folder, ensure it inherits from
+# Combinator and define the applicationClass in the extractor itself.
+#
+# Override the combine and the base_value methods and you're ready to
+# go.
+class Combinator
+
+  # Returns the base_value which is used to combine the first result.
+  def base_value
+    raise "#{self.name} should define a none_value."
+  end
+
+  # Combines the first value with the newly extracted information.
+  def combine( base, extracted_information )
+    raise "#{self.name} should override the combine method."
+  end
+
+end
