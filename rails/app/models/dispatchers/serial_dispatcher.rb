1318300bddc1439c1a45c863752497e3e129b493
Aad Versteden <aad@knowified.com>
2014-07-03 09:55:39 +0200

    Introducing a serial disptacher which dispatches each request in series.

---
+++ rails/app/models/dispatchers/serial_dispatcher.rb
@@ -0,0 +1,15 @@
+class Dispatchers::SerialDispatcher < Dispatcher
+
+  # The default behaviour of Dispatcher is to handle each friend in series
+  def dispatch_friend( friend )
+    uri = URI "#{friend.url}#{plugin.request.path}"
+    Net::HTTP.get( uri.host , uri.path ) # get is overly eager
+  end
+
+  # The default behaviour of Dispatcher is to handle each kitten in series
+  def dispatch_kitten( kitten )
+    uri = URI "#{kitten.url}#{plugin.request.path}"
+    Net::HTTP.get( uri.host , uri.path ) # get is overly eager
+  end
+
+end
