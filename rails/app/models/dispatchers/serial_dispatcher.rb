0e4c7a725f5f624d49848b49cd71b6b506f96a92
Aad Versteden <aad@knowified.com>
2014-07-03 12:37:47 +0200

    don't send the path with the title request (we could do it, but we needn't do it right now)

--- rails/app/models/dispatchers/serial_dispatcher.rb
+++ rails/app/models/dispatchers/serial_dispatcher.rb
@@ -2,13 +2,13 @@ class Dispatchers::SerialDispatcher < Dispatcher
 
   # The default behaviour of Dispatcher is to handle each friend in series
   def dispatch_friend( friend )
-    uri = URI "#{friend.url}#{plugin.request.path}"
+    uri = URI "#{friend.url}/"
     Net::HTTP.get( uri.host , uri.path ) # get is overly eager
   end
 
   # The default behaviour of Dispatcher is to handle each kitten in series
   def dispatch_kitten( kitten )
-    uri = URI "#{kitten.url}#{plugin.request.path}"
+    uri = URI "#{kitten.url}/"
     Net::HTTP.get( uri.host , uri.path ) # get is overly eager
   end
 
