46a540a1a940a5084e152714647aab0e8bb4f047
Aad Versteden <madnificent@gmail.com>
2014-07-08 11:55:47 +0200

    Showing the verb and regex in the plugin's listing for easier understanding of the published paths

--- ember/app/models/plugin.coffee
+++ ember/app/models/plugin.coffee
@@ -1,6 +1,8 @@
 `import DS from 'ember-data'`
 
 Plugin = DS.Model.extend
-  url: DS.attr('string')
+  url: DS.attr    'string'
+  requestMethod: DS.attr 'string'
+  requestRegex:  DS.attr 'string'
 
 `export default Plugin`
--- rails/app/serializers/plugin_serializer.rb
+++ rails/app/serializers/plugin_serializer.rb
@@ -1,8 +1,16 @@
 class PluginSerializer < ActiveModel::Serializer
-  attributes :url, :id
+  attributes :url, :id, :request_method, :request_regex
 
   def id
     url
+  end
+
+  def request_method
+    object.verb.to_s
+  end
+
+  def request_regex
+    object.regex.to_s
   end
 
 end
