5f3a94e430dc7647255aa164eb86309271b45602
Aad Versteden <madnificent@gmail.com>
2014-09-02 19:15:20 +0200

    adding support for parallel fetching of json documents

---
+++ rails/app/models/dispatchers/parallel_json.rb
@@ -0,0 +1,22 @@
+# Dispatches JSON requests in parallel
+#
+# Sends the requests with a requested JSON request format and parses the request
+# format.  Assumes requests are GET requests.
+class Dispatchers::ParallelDispatcher < Dispatchers::SerialDispatcher
+  include Dispatchers::ParallelDispatcherMixin
+  include HTTParty
+  format :json
+
+  def url_for( node )
+    "#{node.url}#{plugin.path}"
+  end
+
+  def dispatch_friend( friend )
+    get url_for( friend )
+  end
+
+  def dispatch_kitten( kitten )
+    get url_for( kitten )
+  end
+
+end
