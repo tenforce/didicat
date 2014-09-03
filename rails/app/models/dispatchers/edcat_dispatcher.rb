7947f90c6a4f7da3a2a27d6fdce2a8a39afe17ac
Aad Versteden <madnificent@gmail.com>
2014-09-03 17:30:39 +0200

    Introducing a dispatcher specifically for edcat

---
+++ rails/app/models/dispatchers/edcat_dispatcher.rb
@@ -0,0 +1,29 @@
+# Dispatches EDCAT JSON requests in parallel
+#
+# The requests for each node are sent assuming the nodes are the base path
+# for EDCAT instances.
+class Dispatchers::EdcatDispatcher < Dispatchers::SerialDispatcher
+  include Dispatchers::ParallelDispatcherMixin
+
+  def url_for_kitten( node )
+    # strip off edcat/ for the request
+    "#{node.url}#{plugin.request.path[6..-1]}"
+  end
+
+  def url_for_friend( node )
+    "#{node.url}#{plugin.request.path}"
+  end
+
+  def get_json( url )
+    JSON.parse HTTParty.get( url ).body
+  end
+
+  def dispatch_friend( friend )
+    get_json url_for_friend( friend )
+  end
+
+  def dispatch_kitten( kitten )
+    get_json url_for_kitten( kitten )
+  end
+
+end
